2bb27c2be53857c3616b786557d8060e20333c86
Taubah <Taubahpettus@gmailk.com>
2023-06-13 14:28:35 -0500

    Initial Commit

---
+++ QuizCode/QuizCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizCodeApp.swift
+//  QuizCode
+//
+//  Created by Scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
